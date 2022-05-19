12cc177ef1e9948509cb86164228b7631ca1f899
scumbug <6149226+scumbug@users.noreply.github.com>
2022-05-19 13:47:40 +0800

    change server download to linux native

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM ubuntu
+
+RUN dpkg --add-architecture i386 \
+    && apt update \
+    && apt install -y wine64 wine32 curl unzip xvfb winetricks tar
+
+RUN mkdir -p /root/.wine/drive_c/users/root/AppData/LocalLow/'Stunlock Studios'/VRisingServer/Settings \
+    && mkdir /steamcmd \
+    && cd /steamcmd \
+    && curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar xz \
+    && mkdir -p /root/.wine/drive_c/VRisingServer/ \
+    && cd /steamcmd
+
+COPY root .
+
+WORKDIR /scripts
+
+RUN chmod +x ./run.sh
+RUN mkdir /tmp/.X11-unix && chmod 1777 /tmp/.X11-unix
+
+EXPOSE 27015/udp
+EXPOSE 27016/udp
+
+CMD ./run.sh
--- root/scripts/run.sh
+++ root/scripts/run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-cd /root/.wine/drive_c/steamcmd
+cd /steamcmd
 if [ ! -e "./steaminstalled.txt" ]; then
-    wincfg
-    wine steamcmd.exe +force_install_dir "C:\VRisingServer" +login anonymous +app_update 1829350 validate +quit
+    ./steamcmd.sh +force_install_dir "/root/.wine/drive_c/VRisingServer/" +login anonymous +app_update 1829350 validate +quit
     touch ./steaminstalled.txt
 fi
 
 rm -r /tmp/.X0-lock
+
 cd /root/.wine/drive_c/VRisingServer/
-Xvfb :0 -screen 0 1024x768x16 & \
-DISPLAY=:0.0 wine VRisingServer.exe
+Xvfb :1 -screen 0 800x600x24 &
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export DISPLAY=:1
+wine VRisingServer.exe
