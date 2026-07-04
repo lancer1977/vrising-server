634d7bb9bf2d905b5daf51c1dd5e03e6cb5c7dd9
Christopher Richmond <lancer1977@gmail.com>
2026-07-03 20:37:49 -0400

    fix: harden vrising runtime wrapper

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,40 +1,32 @@
 #!/bin/sh
-echo "Setting timezone to $TZ"
-echo $TZ > /etc/timezone 2>&1
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 2>&1
+echo "Setting timezone to ${TZ:-UTC}"
+echo "${TZ:-UTC}" > /etc/timezone 2>&1
+ln -snf "/usr/share/zoneinfo/${TZ:-UTC}" /etc/localtime 2>&1
 dpkg-reconfigure -f noninteractive tzdata 2>&1
 
 server=/home/steam/persistentdata
 settings=/home/steam/settings
 
-if [ ! -z $PUID ]; then
-	usermod -u $PUID steam 2>&1
-fi 
-if [ ! -z $PGID ]; then
-	groupmod -g $PGID steam 2>&1
+if [ -n "${PUID:-}" ]; then
+	usermod -u "$PUID" steam 2>&1
 fi
-if [ -z $SERVERNAME ]; then
+if [ -n "${PGID:-}" ]; then
+	groupmod -g "$PGID" steam 2>&1
+fi
+if [ -z "${SERVERNAME:-}" ]; then
 	SERVERNAME="poly-V"
 fi
-if [ -z $WORLDNAME ]; then
+if [ -z "${WORLDNAME:-}" ]; then
 	WORLDNAME="world1"
 fi
-game_port=""
-if [ ! -z $GAMEPORT ]; then
-	game_port=" -gamePort $GAMEPORT"
-fi
-query_port=""
-if [ ! -z $QUERYPORT ]; then
-	query_port=" -queryPort $QUERYPORT"
-fi
 
-cd $server
+cd "$server"
 if [ ! -e "./steaminstalled.txt" ]; then
-    steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir $server +login anonymous +app_update 1829350 validate +quit
+    steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir "$server" +login anonymous +app_update 1829350 validate +quit
     touch ./steaminstalled.txt
 fi
 
-rm -r /tmp/.X0-lock
+rm -f /tmp/.X0-lock
 
 #cd ~/.wine/drive_c/VRisingServer/
 #Xvfb :1 -screen 0 800x600x24 &
@@ -42,4 +34,12 @@ export WINEDLLOVERRIDES="mscoree,mshtml="
 export DISPLAY=:0
 
 Xvfb :0 -screen 0 800x600x24 &
-wine $server/VRisingServer.exe $settings -serverName "$SERVERNAME" -saveName "$WORLDNAME" -logFile "$settings/VRisingServer.log" "$game_port" "$query_port" 2>&1
+set -- "$settings" -serverName "$SERVERNAME" -saveName "$WORLDNAME" -logFile "$settings/VRisingServer.log"
+if [ -n "${GAMEPORT:-}" ]; then
+	set -- "$@" -gamePort "$GAMEPORT"
+fi
+if [ -n "${QUERYPORT:-}" ]; then
+	set -- "$@" -queryPort "$QUERYPORT"
+fi
+
+wine "$server/VRisingServer.exe" "$@" 2>&1
